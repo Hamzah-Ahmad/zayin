8e63b6032f6e6eefe82bf088a54effa5bd31d0ab
unknown <ahmadhamzah500@gmail.com>
2019-10-20 00:29:10 -0500

    Added more features on gues home page

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -2,22 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 import { Alert, Container } from "reactstrap";
 import PostList from "../post/PostList";
+import GuestPage from "./GuestPage";
 
 const Home = props => {
   const { isAuthenticated } = props.auth;
 
   // Use this is the homepage if user not authenticated
-  const guestPage = (
-    <div className="guestPage">
-      <h1 className="guestPageTitle">Welcome to Zayin</h1>
-      <p className="guestPagePara">
-        Talk with the internet about your favourite topics.
-      </p>
-    </div>
-  );
+  // const guestPage = (
+  //   <div className="guestPage">
+  //     <h1 className="guestPageTitle">Welcome to Zayin</h1>
+  //     <p className="guestPagePara">
+  //       Talk with the internet about your favourite topics.
+  //     </p>
+  //   </div>
+  // );
+  const guestPage = <GuestPage />;
 
   // Use this is the homepage if user is authenticated
   const homePage = <PostList />;
+
   return (
     <div>
       <Container>
